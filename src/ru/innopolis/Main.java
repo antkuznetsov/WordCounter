85c29b96d59414099fed23c693a2aa199894c662
Anton Kuznetsov <antkuznetsov@yandex.ru>
2017-04-09 20:49:57 +0300

    Реализована обработка файлов по ссылке из интернета

--- src/ru/innopolis/Main.java
+++ src/ru/innopolis/Main.java
@@ -2,20 +2,32 @@ package ru.innopolis;
 
 import ru.innopolis.models.WordsList;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
 
         WordsList list = new WordsList(); // Создадим базу слов
 
         //Получим список файлов
         for (int i = 0; i < args.length; i++) {
 
-            new WordsCounter(args[i], list);
+            // Если начинается с -l, то ссылка
+            if (args[i].startsWith("-l"))
+            {
+                i++;
+                new WordsCounter(args[i], list, true);
+                //System.out.println(args[i]);
+
+            } else {
+                new WordsCounter(args[i], list);
+                //System.out.println(args[i]);
+            }
 
         }
 
-        Thread.currentThread().sleep(3000);
+        Thread.currentThread().sleep(5000);
 
         list.showResultList(); // Покажем итоговый результат
     }
--- src/ru/innopolis/WordsCounter.java
+++ src/ru/innopolis/WordsCounter.java
@@ -3,6 +3,10 @@ package ru.innopolis;
 import ru.innopolis.models.WordsList;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Scanner;
 
 /**
@@ -31,6 +35,21 @@ public class WordsCounter implements Runnable {
             System.out.println("File not found");
 
         }
+        t.start();
+    }
+
+    public WordsCounter(String fileName, WordsList list, Boolean link) throws IOException {
+
+        this.fileName = fileName;
+        this.list = list;
+
+        URL url = new URL(fileName);
+
+        // Retrieve the InputStream from the URL
+        InputStream is = url.openStream();
+
+        myText = new Scanner(is);
+
         t.start();
     }
 
